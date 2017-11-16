f2e8b648d9acb801629fcd67a9f479b7af51d53a
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-11-16 12:51:27 -0300

    Script para realizar backup de arquivos de sistema

---
+++ backup_diretorios_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Responsavel: Jeferson Araujo
+#Email: jefersonaraujo95@gmail.com
+
+DATA=`date +%Y_%m_%d`
+
+# Dados do arquivo de backup
+MAQUINA="NomeDoServidor"
+ARQUIVO="backup-full-$MAQUINA-$DATA.tar.gz"
+DIRETORIOS="/etc /bin /home /var/www"
+
+
+# Cria o arquivo .tar.gz no /tmp (Temporáo)
+cd /tmp
+tar zcf /tmp/$ARQUIVO $DIRETORIOS
+
+cp /tmp/$ARQUIVO /mnt/backup/$MAQUINA
+
+# Remove os arquivos temporarios
+rm -f /tmp/$ARQUIVO
