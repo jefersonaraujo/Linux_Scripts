63f0ee3ae1725aed193c8f55e150da57058388c9
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-11-16 12:51:44 -0300

    Update

--- snmp-test.sh
+++ snmp-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Responsavel: Jeferson Araujo
+#Email: jefersonaraujo95@gmail.com
+
 comu=$1; host=$2; port=$3
 snmpstatus -v 1 -c $comu $host:$port >/dev/null 2>&1
 if [ ! $? -eq 0 ]; then
