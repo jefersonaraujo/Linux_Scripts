5acfaf51776dc1fc370f7e8f12e1072d97e94653
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-01 12:02:28 -0300

    Criando sistema log

--- verify-badblocks-devices.sh
+++ verify-badblocks-devices.sh
@@ -4,9 +4,15 @@
 
 #parted -l | grep "dev"| awk -F ":" '{print $1" "$2}'
 #parted -l | grep "dev"| awk -F ":" '{print $1" "$2}' | cut -d " " -f2
+DATA=`date +%Y-%m-%d`
 parted -l | grep "dev"| awk -F ":" '{print $1" "$2}' > /tmp/devices
 cat /tmp/devices | cut -d " " -f2
 
-for device in $(cat /tmp/devices | cut -d " " -f2): do
-  badblocks -s -c 10240 /dev/$device >> /var/badblocks.log
+for device in $(cat /tmp/devices | cut -d " " -f2);
+do
+  echo "#####################################################" >> /var/badblocks.log
+  echo "INICIANDO O $device as $DATA" >> /var/badblocks.log
+  badblocks -sv -c 10240 $device >> /var/badblocks.log
+  echo "FINALIZADO O $device as $DATA" >> /var/badblocks.log
+  echo "#####################################################" >> /var/badblocks.log
 done
