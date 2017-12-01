44a1cbc8d57fa447fa3f70da7d3277722c45ffb0
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-01 11:28:32 -0300

    Joga arquivos em arquivo temp

--- fdisk-all-devices.sh
+++ fdisk-all-devices.sh
@@ -2,4 +2,7 @@
 #Responsavel: Jeferson Araujo
 #Email: jefersonaraujo95@gmail.com
 
-parted -l | grep "dev"| awk -F ":" '{print $1" "$2}'
+#parted -l | grep "dev"| awk -F ":" '{print $1" "$2}'
+#parted -l | grep "dev"| awk -F ":" '{print $1" "$2}' | cut -d " " -f2
+parted -l | grep "dev"| awk -F ":" '{print $1" "$2}' > /tmp/devices
+cat /tmp/devices | cut -d " " -f2
