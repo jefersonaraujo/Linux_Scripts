f64b42a555eed07cc99298ecaa1eb5a1562edd76
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-01 12:18:40 -0300

    Script lista todos os devices em /dev e depois verificar se há badblocks, com um pequeno esquema de log para verificação pos procedimento.

--- verify-badblocks-devices.sh
+++ verify-badblocks-devices.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #Responsavel: Jeferson Araujo
 #Email: jefersonaraujo95@gmail.com
-
-#parted -l | grep "dev"| awk -F ":" '{print $1" "$2}'
-#parted -l | grep "dev"| awk -F ":" '{print $1" "$2}' | cut -d " " -f2
-DATA=`date +%Y-%m-%d`
+DATA=`date +%d-%m-%Y-%H:%M:%S`
 parted -l | grep "dev"| awk -F ":" '{print $1" "$2}' > /tmp/devices
 cat /tmp/devices | cut -d " " -f2
 
