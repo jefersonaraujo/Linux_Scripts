eb51153c2e033d7d5c3cc8af0faaf798710275b1
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-05 09:46:47 -0300

    Impletação de log

--- verify-badblocks-devices.sh
+++ verify-badblocks-devices.sh
@@ -3,13 +3,13 @@
 #Email: jefersonaraujo95@gmail.com
 DATA=`date +%d-%m-%Y-%H:%M:%S`
 parted -l | grep "dev"| awk -F ":" '{print $1" "$2}' > /tmp/devices
-cat /tmp/devices | cut -d " " -f2
+#cat /tmp/devices | cut -d " " -f2
 
 for device in $(cat /tmp/devices | cut -d " " -f2);
 do
   echo "#####################################################" >> /var/badblocks.log
-  echo "INICIANDO O $device as $DATA" >> /var/badblocks.log
+  echo "INICIANDO O $device as date +%d-%m-%Y-%H:%M:%S" >> /var/badblocks.log
   badblocks -sv -c 10240 $device >> /var/badblocks.log
-  echo "FINALIZADO O $device as $DATA" >> /var/badblocks.log
+  echo "FINALIZADO O $device as date +%d-%m-%Y-%H:%M:%S" >> /var/badblocks.log
   echo "#####################################################" >> /var/badblocks.log
 done
