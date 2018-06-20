186614dea6d7c6c8bb545b7bbe5d5ee3b79dc990
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-06-20 10:36:18 -0300

    update test

---
+++ rsync_teste.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Responsavel: Jeferson Araujo
+#Email: jefersonaraujo95@gmail.com
+
+DATA=`date +%Y_%m_%d`
+
+# Dados do arquivo de backup
+MAQUINA="NomeDoServidor"
+ARQUIVO="backup-full-$MAQUINA-$DATA.tar.gz"
+DIRETORIOS="/etc /bin /home /var/www /opt"
+#Script realiza apenas backup de arquivos modificados
+
+# Cria o arquivo .tar.gz no /tmp (Temporario)
+cd /tmp
+find $DIRETORIOS -mtime -1 -type f -print | \
+tar zcf /tmp/$ARQUIVO -T -
+
+cp /tmp/$ARQUIVO /mnt/backup/ads/
+
+# Remove os arquivos temporarios
+rm -f /tmp/$ARQUIVO
