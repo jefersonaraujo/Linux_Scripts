93e76f8366543abcc10a63780e1e573522f9db29
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-06-20 09:07:50 -0300

    update test

--- backup_diretorios_linux.sh
+++ backup_diretorios_linux.sh
@@ -3,18 +3,19 @@
 #Email: jefersonaraujo95@gmail.com
 
 DATA=`date +%Y_%m_%d`
-
+m=`findmnt /mnt/pen | wc -l`
+echo $m
 # Dados do arquivo de backup
-MAQUINA="NomeDoServidor"
-ARQUIVO="backup-full-$MAQUINA-$DATA.tar.gz"
-DIRETORIOS="/etc /bin /home /var/www"
+# MAQUINA="NomeDoServidor"
+# ARQUIVO="backup-full-$MAQUINA-$DATA.tar.gz"
+# DIRETORIOS="/etc /bin /home /var/www"
 
 
 # Cria o arquivo .tar.gz no /tmp (Temporáo)
-cd /tmp
-tar zcf /tmp/$ARQUIVO $DIRETORIOS
-
-cp /tmp/$ARQUIVO /mnt/backup/$MAQUINA
+# cd /tmp
+# tar zcf /tmp/$ARQUIVO $DIRETORIOS
+#
+# cp /tmp/$ARQUIVO /mnt/backup/$MAQUINA
 
 # Remove os arquivos temporarios
-rm -f /tmp/$ARQUIVO
+# rm -f /tmp/$ARQUIVO
