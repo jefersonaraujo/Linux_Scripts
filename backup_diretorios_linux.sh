5221f27f84c17e2d0dec41c7e2080835f85a8d17
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-06-20 10:14:41 -0300

    update test

--- backup_diretorios_linux.sh
+++ backup_diretorios_linux.sh
@@ -3,26 +3,27 @@
 #Email: jefersonaraujo95@gmail.com
 
 DATA=`date +%Y_%m_%d`
-m=`findmnt /mnt/pen | wc -l`
-echo $m
-if [ $m -eq 0 ]; then
-echo -e "FALHA"
-exit 1
-else
-echo -e "OK"
-fi
-exit 0
+m=`findmnt /mnt/backup | wc -l`
+
+if [ ! $m -eq 0 ]; then
+echo -e "faca"
 # Dados do arquivo de backup
-# MAQUINA="NomeDoServidor"
-# ARQUIVO="backup-full-$MAQUINA-$DATA.tar.gz"
-# DIRETORIOS="/etc /bin /home /var/www"
+MAQUINA="NomeDoServidor"
+ARQUIVO="backup-full-$MAQUINA-$DATA.tar.gz"
+DIRETORIOS="/etc /bin /home /var/www"
 
 
 # Cria o arquivo .tar.gz no /tmp (Temporáo)
-# cd /tmp
-# tar zcf /tmp/$ARQUIVO $DIRETORIOS
-#
-# cp /tmp/$ARQUIVO /mnt/backup/$MAQUINA
+cd /tmp
+tar zcf /tmp/$ARQUIVO $DIRETORIOS
 
-# Remove os arquivos temporarios
-# rm -f /tmp/$ARQUIVO
+cp /tmp/$ARQUIVO /mnt/backup/$MAQUINA
+
+Remove os arquivos temporarios
+rm -f /tmp/$ARQUIVO
+exit 1
+else
+hora='date'
+echo -e "Particao de Backup não montada. - $hora" >> /var/log/backup.log
+fi
+exit 0
